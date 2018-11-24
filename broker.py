01054ef3ed630fc06e2feee5e2d9924e3b93893f
fatihdev <fatihdev@b.lab0fd.ch-geni-net.instageni.cenic.net>
2018-11-24 11:39:58 -0800

    Finalized broker

--- broker.py
+++ broker.py
@@ -4,9 +4,16 @@ from datetime import datetime
 from time import sleep
 
 
-source_port = 2237
-r1_port = 2224
-r2_port = 2225
+source_port = 2239 # Receiving port
+# Sending ports
+r1_port = 2234
+r2_port = 2235
+
+r1_name = '10.10.3.1'
+r2_name = '10.10.4.2'
+
+message_size = 256 # Predefined packet size
+
 source_socket = socket(AF_INET,SOCK_STREAM)
 source_socket.bind(("",source_port))
 source_socket.listen(1)
@@ -14,17 +21,14 @@ print 'Broker is ready to receive'
 
 connection_socket, addr = source_socket.accept()
 
-r1_name = '172.17.2.18'
-r2_name = '172.17.2.19'
-
 r1_socket = socket(AF_INET, SOCK_DGRAM)
 r2_socket = socket(AF_INET, SOCK_DGRAM)
 
-for i in range(0,376):
-	message = connection_socket.recv(2048)
+for i in range(0,200):
+	message = connection_socket.recv(message_size)
 	r1_socket.sendto(message, (r1_name, r1_port))
 	r2_socket.sendto(message, (r2_name, r2_port))
-	
+
 r1_socket.close()
 r2_socket.close()
 source_socket.close()
