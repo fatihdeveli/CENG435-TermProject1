f1c2d51d7070431afcb056bf02b5813be9ac06d8
root <root@r2.lab0fd.ch-geni-net.instageni.cenic.net>
2018-11-24 11:34:56 -0800

    Finalized router2

--- router2.py
+++ router2.py
@@ -1,9 +1,11 @@
 from socket import *
 
-destination = '172.17.2.16' #
+destination = '10.10.5.2' # Destination address
 
-broker_port = 2225 # receive
-destination_port = 12002 # send
+broker_port = 2235 # Receiving port (broker)
+destination_port = 12002 # Sending port (destination)
+
+message_size = 256 # Predefined packet size
 
 broker_socket = socket(AF_INET, SOCK_DGRAM)
 broker_socket.bind(('', broker_port))
@@ -12,6 +14,8 @@ destination_socket = socket(AF_INET, SOCK_DGRAM)
 
 print 'Router 2 is ready'
 while 1:
-    message, broker_address = broker_socket.recvfrom(2048)
-    destination_socket.sendto(message, (destination, destination_port))
-
+	try:
+		message, broker_address = broker_socket.recvfrom(message_size)
+	except(KeyboardInterrupt):
+		exit()
+	destination_socket.sendto(message, (destination, destination_port))
