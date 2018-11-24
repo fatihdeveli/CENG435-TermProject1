c78047589143bdb6594d4a3e263e31e04ba3c80f
fatihdev <fatihdev@s.lab0fd.ch-geni-net.instageni.cenic.net>
2018-11-24 11:44:23 -0800

    Finalized source

--- source.py
+++ source.py
@@ -2,23 +2,29 @@ from socket import *
 from time import sleep
 from datetime import datetime
 
-f = open("A Tale of Two Cities.txt"); # Obtain the sensor readings
-sensor = ''
+f = open("A Tale of Two Cities.txt"); # Source of sensor readings
+sensor = '' # Variable to hold sensor readings
+
 for line in f:
     sensor = sensor + line
 f.close()
 
-server_name = '172.17.2.15' # IP address of broker
-server_port = 2237
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_name,server_port))
-payload_size = 2048-27 # characters
+broker_name = '10.10.4.1' # IP address of broker
+broker_port = 2239
 
-length = len(sensor)
+message_size = 256 # Predefined packet size
 
-for i in range(0, length, payload_size):
+broker_socket = socket(AF_INET, SOCK_STREAM)
+broker_socket.connect((broker_name, broker_port))
+
+payload_size = message_size-27 # Timestamp is 27 characters.
+
+message_counter = payload_size*200 # 200 packets will be sent.
+
+for i in range(0, message_counter, payload_size):
+	# Add timestamp to the message
 	message = str(datetime.utcnow()) + '~' + sensor[i:i+payload_size]
-	client_socket.send(message)
-	sleep(0.05)
+	broker_socket.send(message)
+	sleep(0.3) # Wait for the messages to be processed in the network.
 
-client_socket.close()
+broker_socket.close()
